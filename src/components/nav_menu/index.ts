b0a57ee4468cd753435968e7f74311f163c28259
ceaser <2902465304@qq.com>
2022-08-04 21:14:23 +0800

    feat: main cpn added

---
+++ src/components/nav_menu/index.ts
@@ -0,0 +1,3 @@
+import nav_menu from './src/nav_menu.vue'
+
+export default nav_menu
---
+++ src/components/nav_menu/src/nav_menu.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="nav-menu">
+    <div class="logo">
+      <img src="~@/assets/logo.png" alt="logo" />
+      <span class="title">Vue3+TS</span>
+    </div>
+
+    <el-menu default-active="2" class="el-menu-vertical-demo">
+      <template v-for="item in user_menus" :key="item.id">
+        <template v-if="item.type === 1">
+          <el-sub-menu :index="item.id + ''">
+            <template #title>
+              <i v-if="item.icon" :class="item.icon"></i>
+              <el-icon><BottomRight /></el-icon>
+              <span>{{ item.name }}</span>
+            </template>
+            <template v-for="subitem in item.children" :key="subitem.id">
+              <el-menu-item :index="subitem.id + ''">
+                <i v-if="subitem.icon" :class="subitem.icon"></i>
+                <span>{{ subitem.name }}</span>
+              </el-menu-item>
+            </template>
+          </el-sub-menu>
+        </template>
+        <template v-else>
+          <el-menu-item :index="item.id + ''">
+            <i v-if="item.icon" :class="item.icon"></i>
+            <span>{{ item.name }}111</span>
+          </el-menu-item>
+        </template>
+      </template>
+    </el-menu>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default defineComponent({
+  setup() {
+    const store = useStore()
+    const user_menus = computed(() => store.state.login.user_menus)
+    return {
+      user_menus
+    }
+  }
+})
+</script>
+
+<style scoped lang="less">
+.nav-menu {
+  height: 100%;
+  background-color: #fff;
+  .logo {
+    display: flex;
+    height: 28px;
+    padding: 12px 10px 8px 10px;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    img {
+      height: 100%;
+      margin: 0 10px;
+    }
+    .title {
+      font-size: 16px;
+      font-weight: 700;
+      color: rgb(48, 49, 51);
+    }
+  }
+  .el-menu {
+    border-right: none;
+  }
+}
+</style>
