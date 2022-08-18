92289f10fbb4002691758a0ea153a0ef32e77e2a
ceaser <2902465304@qq.com>
2022-08-18 21:34:01 +0800

    feat: nav_info cpns new feature added

--- src/components/nav_header/src/nav_header.vue
+++ src/components/nav_header/src/nav_header.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="nav-header">
     <el-icon class="fold-menu" @click="fold_change()"><Operation /></el-icon>
+    <div class="content">
+      <div>面包屑</div>
+      <user_info></user_info>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+import user_info from './user_info.vue'
 
 export default defineComponent({
   emits: ['fold_change'],
+  components: {
+    user_info
+  },
   setup(props, { emit }) {
     const is_fold = ref(false)
     const fold_change = () => {
@@ -25,11 +33,21 @@ export default defineComponent({
 
 <style scoped lang="less">
 .nav-header {
+  display: flex;
+  width: 100%;
   .fold-menu {
     font-size: 35px;
     color: #fff;
     margin-left: 20px;
     cursor: pointer;
+  }
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 20px;
+    color: #fff;
   }
 }
 </style>
---
+++ src/components/nav_header/src/user_info.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <el-dropdown>
+      <span class="el-dropdown-link userinfo">
+        <el-avatar
+          src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+        />
+        <span class="span">{{ name }}</span>
+        <el-icon class="el-icon--right">
+          <arrow-down />
+        </el-icon>
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>Action 1</el-dropdown-item>
+          <el-dropdown-item>Action 2</el-dropdown-item>
+          <el-dropdown-item>Action 3</el-dropdown-item>
+          <el-dropdown-item divided>Action 5</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default defineComponent({
+  setup() {
+    const store = useStore()
+    const name = computed(() => store.state.login.user_info.data.name)
+    return { name }
+  }
+})
+</script>
+
+<style scoped lang="less">
+.userinfo {
+  color: #fff;
+  display: flex;
+  align-items: center;
+  .span {
+    margin-left: 5px;
+  }
+}
+</style>
